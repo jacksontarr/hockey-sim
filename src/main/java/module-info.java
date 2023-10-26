f76c1560b1ac1e1b67470cca0a55bcceb3ef521f
Jackson Tarr <jpt5465@rit.edu>
2023-10-26 15:02:28 -0400

    File structure done

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module HockeySim {
+    // requires transitive javafx.graphics;
+    // requires javafx.controls;
+    // requires javafx.fxml;
+    requires json.simple;
+    requires com.google.gson;
+    requires org.apache.commons.io;
+    // opens HockeySim to javafx.fxml;
+    // exports HockeySim;
+}
